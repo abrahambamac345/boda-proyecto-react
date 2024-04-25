345eb444dab34f581a95183769468959fe72b2df
abrahambamac345 <104279106+abrahambamac345@users.noreply.github.com>
2024-04-25 14:53:39 -0600

    pruebas1234

--- src/photos.jsx
+++ src/photos.jsx
@@ -1,65 +1,61 @@
-// photos.jsx
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { storage } from './firebase-config';
-
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import fotografia from './assets/img/fotografia.png';
 
-function Photos() {
+const storage = getStorage();
+
+function photos() {
   const [file, setFile] = useState(null);
-  const navigate = useNavigate();
 
-  const handleOpenCamera = () => {
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'image/*';
-    input.addEventListener('change', handleFileSelect, false);
-    input.click();
-  };
-
-  const handleFileSelect = (event) => {
+  const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     setFile(selectedFile);
   };
 
-  const handleUpload = async () => {
-    if (!file) {
-      alert('Selecciona una imagen primero.');
-      return;
+  const handleUpload = () => {
+    if (file) {
+      const storageRef = ref(storage, `uploads/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+  
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          // Obtener el progreso de carga como un porcentaje
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Progreso de carga:', progress + '%');
+        },
+        (error) => {
+          console.error('Error al cargar el archivo:', error);
+        },
+        () => {
+          // Carga completada con éxito
+          console.log('Archivo cargado con éxito');
+        }
+      );
     }
-  
-    const storageRef = storage.ref();
-    const imageRef = storageRef.child(`images/${file.name}`);
-  
-    try {
-      console.log('Subiendo imagen...');
-      await imageRef.put(file);
-      console.log('Imagen subida con éxito.');
-  
-      const imageUrl = await imageRef.getDownloadURL();
-      console.log('URL de la imagen:', imageUrl);
-  
-      // Redirigir a TomarFoto.jsx
-      navigate(`/TomarFoto?image=${encodeURIComponent(imageUrl)}`);
-    } catch (error) {
-      console.error('Error al subir la imagen:', error);
-    }
+  };
+
+  const handleOpenCamera = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*, video/*'; // Acepta tanto imágenes como videos
+    input.addEventListener('change', handleFileChange, false);
+    input.click();
   };
 
   return (
     <section className='compartir'>
-    <h1 className='text-vesti' >"Se Parte de Nuestra Historia"</h1>
-    <h2 className='text-fotos' >Comparte una fotografía o un comentario</h2>
-    <div className='fotor'>
-      <img src={fotografia} loading="lazy" alt="imagen de un jardin" className='fotografia'/>
-    </div>
-    <div className='boton-FSC'>
-      <button className='tfotos' onClick={handleOpenCamera}>Tomar Fotos</button>
-      <button className='tfotos' onClick={handleUpload}>Subir Fotos</button>
-      <button className='tfotos'>Comentario</button>
-    </div>
-  </section>
+      <h1 className='text-vesti' >"Se Parte de Nuestra Historia"</h1>
+      <h2 className='text-fotos' >Comparte una fotografía o un comentario</h2>
+      <div className='fotor'>
+        <img src={fotografia} loading="lazy" alt="imagen de un jardin" className='fotografia'/>
+      </div>
+      <div className='boton-FSC'>
+        <button className='tfotos' onClick={handleOpenCamera}>Tomar Fotos o Grabar Video</button>
+        <button className='tfotos' onClick={handleUpload}>Subir Archivo</button>
+        <button className='tfotos'>Comentario</button>
+      </div>
+    </section>
   );
 }
 
-export default Photos;
+export default photos;
