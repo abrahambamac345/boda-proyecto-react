117ddac7784ba20776c38dcba4fefbe41dfd4bef
abrahambamac345 <104279106+abrahambamac345@users.noreply.github.com>
2024-04-26 00:32:48 -0600

    papapas

--- src/FotoMarco.jsx
+++ src/FotoMarco.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import { saveAs } from 'file-saver';
 
 function FotoMarco() {
   const location = useLocation();
-  const imageUrl = new URLSearchParams(location.search).get('url');
+  const url = new URLSearchParams(location.search).get('url');
 
-  const downloadPhoto = async () => {
-    try {
-      // Descargar la imagen utilizando FileSaver.js
-      saveAs(imageUrl, 'nombre_de_la_imagen.png'); // Cambia 'nombre_de_la_imagen.png' por el nombre que desees para el archivo descargado
-    } catch (error) {
-      console.error('Error al descargar la foto:', error);
-    }
+  const downloadPhoto = () => {
+    if (!url) return;
+
+    // Crear un enlace temporal para descargar la imagen
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'foto_original.jpg'; // Nombre del archivo a descargar
+
+    // Simular un clic en el enlace para iniciar la descarga
+    link.click();
   };
 
   return (
     <div>
       <h1 className='llos'>¡Aquí está tu foto!</h1>
-      {imageUrl && (
+      {url && (
         <div>
-          <img src={imageUrl} alt="Tu foto" className="foto-original" />
+          <img src={url} alt="Tu foto" className="foto-original" />
           <button className="button-descargar" onClick={downloadPhoto}>Descargar Foto</button>
         </div>
       )}
